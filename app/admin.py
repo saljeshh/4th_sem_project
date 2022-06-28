685ff23f747475eec06468a8e40ff072dad342a0
Saljesh <saljesh2020@gmail.com>
2022-06-28 12:41:24 +0545

    final project

---
+++ app/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+@admin.register(Customer)
+class CustomerModelAdmin(admin.ModelAdmin):
+    list_display = ['id','user','name','locality','city','zipcode','state']
+
+
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = ['id','title','selling_price','discounted_price','description','brand','category','product_image']
+
+
+@admin.register(Cart)
+class CartModelAdmin(admin.ModelAdmin):
+    list_display = ['id','user','product','quantity']
+
+
+@admin.register(OrderPlaced)
+class OrderedPlacedModelAdmin(admin.ModelAdmin):
+    list_display = ['id','user','customer','product', 'quantity','ordered_date','status']
