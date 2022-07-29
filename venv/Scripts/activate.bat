3a409888984ba4038ef2f69d4f02457dd22512f0
saljeshh <saljesh2020@gmail.com>
2022-07-29 10:29:07 +0545

    updated venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=P:\Coding\04_django\Ecommerce\venv
+set VIRTUAL_ENV=S:\4th_sem_project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
